24562a6eb1dd54641c09853ff11d3deb5c219044
AndreyMamonov <ayumamonov@beeline.ru>
2024-09-24 23:58:29 +0300

    Added backend integration

--- app/src/main/java/com/example/myapplication/di/AppModule.kt
+++ app/src/main/java/com/example/myapplication/di/AppModule.kt
@@ -1,14 +1,23 @@
 package com.example.myapplication.di
 
-import com.example.data.repository.ListRepositoryImpl
+import com.example.data.network.Api
+import com.example.data.repository.NetworkRepository
 import com.example.domain.repository.ListRepository
 import com.example.domain.usecase.ListUseCase
 import com.example.myapplication.main.vm.MainViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val appModule = module {
-    single<ListRepository> { ListRepositoryImpl() }
+    single {
+        Retrofit.Builder()
+            .baseUrl("https://mocki.io")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(Api::class.java)
+    }
+    single<ListRepository> { NetworkRepository(get()) }
     single { ListUseCase(get()) }
     viewModel { MainViewModel(get()) }
 }
