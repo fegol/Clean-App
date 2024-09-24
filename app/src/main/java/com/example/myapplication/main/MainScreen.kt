645ac1920ca3c8431cff82851506bfb9ee44aa84
AndreyMamonov <ayumamonov@beeline.ru>
2024-09-24 23:47:23 +0300

    Added coil

--- app/src/main/java/com/example/myapplication/main/MainScreen.kt
+++ app/src/main/java/com/example/myapplication/main/MainScreen.kt
@@ -2,13 +2,21 @@ package com.example.myapplication.main
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.domain.entity.ListElement
 import com.example.myapplication.main.vm.MainState
 import com.example.myapplication.main.vm.MainViewModel
@@ -43,7 +51,16 @@ fun ErrorState(message: String) {
 fun ContentState(list: List<ListElement>) {
     Column(modifier = Modifier.fillMaxSize()) {
         list.forEach { element ->
-            Text(text = element.title)
+            Row(modifier = Modifier.padding(vertical = 8.dp)) {
+                AsyncImage(
+                    modifier = Modifier.size(136.dp),
+                    model = element.image,
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.width(24.dp))
+                Text(text = element.title)
+            }
         }
     }
 }
